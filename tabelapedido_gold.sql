f4d43ae7338baf336fe38d7f1bc5255a68c3fb30
Yasmin Macena <yasmacx@gmail.com>
2026-08-18 16:35:51 -0300

    Add data insertion for tabelapedido_gold

--- tabelapedido_gold.sql
+++ tabelapedido_gold.sql
@@ -8,3 +8,19 @@ CREATE table tabelapedido_gold(
   FOREIGN KEY (cliente_gold) REFERENCES tabelacliente(id_clientes)
 );
   
+INSERT into tabelapedido_gold
+(id_pedido_gold,
+ data_do_pedido_gold,
+ status_gold,
+ total_pedido_gold,
+ cliente_gold,
+ data_envio_estimada_gold)
+ SELECT
+id_pedido,
+data_do_pedido,
+status_pedido,
+total_pedido,
+nome_cliente_produto,
+data_do_pedido
+from tabela_pedidos
+where total_pedido >= 400
