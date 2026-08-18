c4efa454ec7b01c1fe60a50ae392ee99791124cd
Yasmin Macena <yasmacx@gmail.com>
2026-08-18 16:36:16 -0300

    Insert initial product data into tabela_produtos
    Added initial product entries to tabela_produtos.

--- tabelaprodutos.sql
+++ tabelaprodutos.sql
@@ -11,4 +11,16 @@ CREATE TABLE tabela_produtos (
   FOREIGN KEY (fornecedor) REFERENCES tabelafornecedores(id)
   );
   
-  
+  INSERT into tabela_produtos(
+id_produtos,
+  nome_produto, 
+  descricao_produto,
+  categoria_produto,
+  preco_produto,
+  unidade_produto,
+  fornecedor,
+  data_de_inclusao)
+  VALUES
+(1, 'Smartphone X', 'Smartphone de última geração', 1, 699.99, 'Unidade', 1, '2023-08-01'),
+(2, 'Notebook Pro', 'Notebook poderoso com tela HD', 2, 1199.99, 'Unidade', 2, '2023-08-02'),
+(3, 'Tablet Lite', 'Tablet compacto e leve', 3, 299.99, 'Unidade', 3, '2023-08-03');
