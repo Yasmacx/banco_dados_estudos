6ae3be6bbe959beebd4c3408827af5bf2429f3cc
Yasmin Macena <yasmacx@gmail.com>
2026-08-18 16:37:19 -0300

    Insert new client records into tabela_clientes
    Added insert statements for new client records.

--- tabela_clientes.sql
+++ tabela_clientes.sql
@@ -4,3 +4,13 @@ CREATE TABLE tabelaclientes (
   Info_contato VARCHAR (250)
   );
   SELECT*FROM tabelaclientes;
+
+INSERT into tabelacliente
+(id_clientes,
+ nome_cliente,
+ info_contato,
+ endereco_clientes)
+ VALUES 
+ ('2','João Santos','joao.santos@email.com','rua dos pinheiros, 234'),
+ ('3','Julia Alves','julia.alves@haoc.com','rua das cachimbas,1'),
+ ('4','Rhuan Santos','rhuan.santos@gmail.com','rua pelicanos, 123')
